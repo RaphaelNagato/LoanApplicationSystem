e92129ed9fa8f8a253d4c1452e0319d0b23a2f62
RaphaelNagato <raphaelnagato19@gmail.com>
2025-06-26 05:54:31 +0100

    delete unnecessary files

--- Infrastructure/Services/LoanApplicationService.cs
+++ Infrastructure/Services/LoanApplicationService.cs
@@ -6,22 +6,27 @@ namespace Infrastructure.Services;
 internal class LoanApplicationService(ILoanRepository loanRepository) : ILoanApplicationService
 {
     private readonly ILoanRepository _loanRepository = loanRepository;
+
     public async Task<LoanApplication?> GetByIdAsync(int id, CancellationToken ct)
     {
         return await _loanRepository.GetByIdAsync(id, ct);
     }
+
     public async Task<IEnumerable<LoanApplication>> GetAllAsync(CancellationToken ct)
     {
         return await _loanRepository.GetAllAsync(ct);
     }
+
     public async Task AddAsync(LoanApplication loanApplication, CancellationToken ct)
     {
         await _loanRepository.AddAsync(loanApplication, ct);
     }
+
     public async Task UpdateAsync(LoanApplication loanApplication, CancellationToken ct)
     {
         await _loanRepository.UpdateAsync(loanApplication, ct);
     }
+
     public async Task DeleteAsync(int id, CancellationToken ct)
     {
         await _loanRepository.DeleteAsync(id, ct);
