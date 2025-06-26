f16e35317c29e04522614d57e387f683dca67544
RaphaelNagato <raphaelnagato19@gmail.com>
2025-06-26 02:10:15 +0100

    Fix prokect folders and addd LoanApplication entity

---
+++ Core/Entities/LoanApplication.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities;
+
+public class LoanApplication
+{
+    [Key]
+    public int Id { get; set; }
+    [Required, MaxLength(255)]
+    public string ApplicantName { get; set; }
+    [Range(5, 1000000)]
+    public decimal LoanAmount { get; set; }
+    public int LoanTermMonths { get; set; }
+    [Range(0, 100)]
+    public decimal InterestRate { get; set; }
+    public LoanApplicationStatus LoanStatus { get; set; }
+    public DateTime ApplicationDate { get; set; }
+}
+
+public enum LoanApplicationStatus
+{
+    Pending,
+    Approved,
+    Rejected
+}
---
+++ LoanApplication.cs
@@ -0,0 +1,19 @@
+﻿public class LoanApplication
+{
+    [Key]
+    public int Id { get; set; }
+    [Required, MaxLength()]
+    public string ApplicantName { get; set; }
+    public decimal LoanAmount { get; set; }
+    public int LoanTermMonths { get; set; }
+    public decimal InterestRate { get; set; }
+    public LoanApplicationStatus Status { get; set; }
+    public DateTime ApplicationDate { get; set; }
+}
+
+public enum LoanApplicationStatus
+{
+    Pending,
+    Approved,
+    Rejected
+}
