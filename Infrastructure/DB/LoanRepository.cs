92063dde1c827a2a9773ae4007ddf427a058e13f
RaphaelNagato <raphaelnagato19@gmail.com>
2025-06-26 03:45:14 +0100

    Add repo and service

---
+++ Infrastructure/DB/LoanRepository.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DB;
+
+internal class LoanRepository(LoanDbContext context) : ILoanRepository
+{
+    private readonly LoanDbContext _context = context ?? throw new ArgumentNullException(nameof(context));
+    public async Task<LoanApplication?> GetByIdAsync(int id, CancellationToken ct)
+    {
+        return await _context.LoanApplications.FindAsync([id], ct);
+    }
+    public async Task<IEnumerable<LoanApplication>> GetAllAsync(CancellationToken ct)
+    {
+        return await _context.LoanApplications.ToListAsync(ct);
+    }
+    public async Task AddAsync(LoanApplication loanApplication, CancellationToken ct)
+    {
+        await _context.LoanApplications.AddAsync(loanApplication, ct);
+        await _context.SaveChangesAsync(ct);
+    }
+    public async Task UpdateAsync(LoanApplication loanApplication, CancellationToken ct)
+    {
+        _context.LoanApplications.Update(loanApplication);
+        await _context.SaveChangesAsync(ct);
+    }
+    public async Task DeleteAsync(int id, CancellationToken ct)
+    {
+        var loanApplication = await GetByIdAsync(id, ct);
+        if (loanApplication != null)
+        {
+            _context.LoanApplications.Remove(loanApplication);
+            await _context.SaveChangesAsync(ct);
+        }
+    }
+}
---
+++ Infrastructure/Services/ILoanApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Infrastructure.Services;
+
+public interface ILoanApplicationService
+{
+    Task AddAsync(LoanApplication loanApplication, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<IEnumerable<LoanApplication>> GetAllAsync(CancellationToken ct);
+    Task<LoanApplication?> GetByIdAsync(int id, CancellationToken ct);
+    Task UpdateAsync(LoanApplication loanApplication, CancellationToken ct);
+}
